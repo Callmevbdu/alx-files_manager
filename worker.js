5fbc7fb0058899b7a189fbe02614b661be52d2da
Abdellatif <abdelt.2012@gmail.com>
2024-08-04 00:35:50 +0100

    Done.

--- worker.js
+++ worker.js
@@ -1,11 +1,14 @@
-import DBClient from './utils/db';
+import { writeFile } from 'fs';
+import { promisify } from 'util';
+import Queue from 'bull/lib/queue';
+import imgThumbnail from 'image-thumbnail';
+import mongoDBCore from 'mongodb/lib/core';
+import dbClient from './utils/db';
+import Mailer from './utils/mailer';
 
-const Bull = require('bull');
-const { ObjectId } = require('mongodb');
-const imageThumbnail = require('image-thumbnail');
-const fs = require('fs');
-
-const fileQueue = new Bull('fileQueue');
+const writeFileAsync = promisify(writeFile);
+const fileQueue = new Queue('thumbnail generation');
+const userQueue = new Queue('email sending');
 
 /**
  * By using the module Bull, create a queue fileQueue
@@ -15,40 +18,72 @@ const fileQueue = new Bull('fileQueue');
  * - If no document is found in DB based on the fileId and userId, raise an error File not found
  * - By using the module image-thumbnail, generate 3 thumbnails with width = 500, 250 and 100 -
  * store each result on the same location of the original file by appending _<width size>
- * @param {String} path - Location of the file.
- * @param {number} options - Thumbnail width.
- * @returns {Promise<void>}
- */
-const createImageThumbnail = async (path, options) => {
-  try {
-    const thumbnail = await imageThumbnail(path, options);
-    const pathNail = `${path}_${options.width}`;
-
-    await fs.writeFileSync(pathNail, thumbnail);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-/**
  * Update the endpoint GET /files/:id/data to accept a query parameter size:
  * - size can be 500, 250 or 100
  * - Based on size, return the correct local file
  * - If the local file doesn’t exist, return an error Not found with a status code 404
+ * @param {String} path - Location of the file.
+ * @param {number} options - Thumbnail width.
+ * @returns {Promise<void>}
  */
-fileQueue.process(async (job) => {
-  const { fileId } = job.data;
-  if (!fileId) throw Error('Missing fileId');
+const generateThumbnail = async (filePath, size) => {
+  const buffer = await imgThumbnail(filePath, { width: size });
+  console.log(`Generating file: ${filePath}, size: ${size}`);
+  return writeFileAsync(`${filePath}_${size}`, buffer);
+};
 
-  const { userId } = job.data;
-  if (!userId) throw Error('Missing userId');
+fileQueue.process(async (job, done) => {
+  const fileId = job.data.fileId || null;
+  const userId = job.data.userId || null;
 
-  const fileDocument = await DBClient.db
-    .collection('files')
-    .findOne({ _id: ObjectId(fileId), userId: ObjectId(userId) });
-  if (!fileDocument) throw Error('File not found');
+  if (!fileId) {
+    throw new Error('Missing fileId');
+  }
+  if (!userId) {
+    throw new Error('Missing userId');
+  }
+  console.log('Processing', job.data.name || '');
+  const file = await (await dbClient.filesCollection())
+    .findOne({
+      _id: new mongoDBCore.BSON.ObjectId(fileId),
+      userId: new mongoDBCore.BSON.ObjectId(userId),
+    });
+  if (!file) {
+    throw new Error('File not found');
+  }
+  const sizes = [500, 250, 100];
+  Promise.all(sizes.map((size) => generateThumbnail(file.localPath, size)))
+    .then(() => {
+      done();
+    });
+});
 
-  createImageThumbnail(fileDocument.localPath, { width: 500 });
-  createImageThumbnail(fileDocument.localPath, { width: 250 });
-  createImageThumbnail(fileDocument.localPath, { width: 100 });
+userQueue.process(async (job, done) => {
+  const userId = job.data.userId || null;
+
+  if (!userId) {
+    throw new Error('Missing userId');
+  }
+  const user = await (await dbClient.usersCollection())
+    .findOne({ _id: new mongoDBCore.BSON.ObjectId(userId) });
+  if (!user) {
+    throw new Error('User not found');
+  }
+  console.log(`Welcome ${user.email}!`);
+  try {
+    const mailSubject = 'Welcome to ALX-Files_Manager';
+    const mailContent = [
+      '<div>',
+      '<h3>Hello {{user.name}},</h3>',
+      'Welcome to <a href="https://github.com/callmevbdu/alx-files_manager">',
+      'ALX-Files_Manager</a>, ',
+      'a simple file management API built with Node.js by ',
+      '<a href="https://github.com/callmevbdu">Abdellatif Hmiche</a>. ',
+      '</div>',
+    ].join('');
+    Mailer.sendMail(Mailer.buildMessage(user.email, mailSubject, mailContent));
+    done();
+  } catch (err) {
+    done(err);
+  }
 });
