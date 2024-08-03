c9ad2fdd1b9f8ceea6fb1b4e393b2ca54d3a21ab
Abdellatif <abdelt.2012@gmail.com>
2024-08-03 22:01:41 +0100

    Done.

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,19 +7,19 @@ class AuthController {
   /**
    * GET /connect should sign-in the user by generating a new authentication
    * token:
-   * 	- By using the header Authorization and the technique of the Basic auth
-   * 	(Base64 of the <email>:<password>), find the user associate to this
-   * 	email and with this password (reminder: we are storing the SHA1 of the
-   * 	password)
-   * 	- If no user has been found, return an error Unauthorized with a status
-   * 	code 401
-   * 	- Otherwise:
-   * 		+ Generate a random string (using uuidv4) as token
-   * 		+ Create a key: auth_<token>
-   * 		+ Use this key for storing in Redis (by using the redisClient
-   * 		create previously) the user ID for 24 hours00
-   * 		+ Return this token: { "token": "155342df-2399-41da-9e8c-458b6ac52a0c" }
-   * 		with a status code 200
+   * - By using the header Authorization and the technique of the Basic auth
+   * (Base64 of the <email>:<password>), find the user associate to this
+   * email and with this password (reminder: we are storing the SHA1 of the
+   * password)
+   * - If no user has been found, return an error Unauthorized with a status
+   * code 401
+   * - Otherwise:
+   * + Generate a random string (using uuidv4) as token
+   * + Create a key: auth_<token>
+   * + Use this key for storing in Redis (by using the redisClient
+   * create previously) the user ID for 24 hours00
+   * + Return this token: { "token": "155342df-2399-41da-9e8c-458b6ac52a0c" }
+   * with a status code 200
    */
   static async getConnect(req, res) {
     const authorization = req.header('Authorization') || null;
@@ -49,10 +49,10 @@ class AuthController {
 
   /**
    * GET /disconnect should sign-out the user based on the token:
-   *  - Retrieve the user based on the token:
-   *	+ If not found, return an error Unauthorized with a status code 401
-   *	+ Otherwise, delete the token in Redis and return nothing with a status
-   *	code 204
+   * - Retrieve the user based on the token:
+   * + If not found, return an error Unauthorized with a status code 401
+   * + Otherwise, delete the token in Redis and return nothing with a status
+   * code 204
    */
   static async getDisconnect(req, res) {
     const token = req.header('X-Token') || null;
