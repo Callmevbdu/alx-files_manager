50c2316c849286a966691499a392f2ec7f2cfb48
Abdellatif <abdelt.2012@gmail.com>
2024-08-03 21:58:06 +0100

    Done.

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -8,19 +8,19 @@ class UsersController {
   /**
    * GET /connect should sign-in the user by generating a new authentication
    * token:
-   * 	- By using the header Authorization and the technique of the Basic auth
-   * 	(Base64 of the <email>:<password>), find the user associate to this
-   * 	email and with this password (reminder: we are storing the SHA1 of the
-   * 	password)
-   * 	- If no user has been found, return an error Unauthorized with a status
-   * 	code 401
-   * 	- Otherwise:
-   * 		+ Generate a random string (using uuidv4) as token
-   * 		+ Create a key: auth_<token>
-   * 		+ Use this key for storing in Redis (by using the redisClient
-   * 		create previously) the user ID for 24 hours
-   * 		+ Return this token: { "token": "155342df-2399-41da-9e8c-458b6ac52a0c" }
-   * 		with a status code 200
+   * - By using the header Authorization and the technique of the Basic auth
+   * (Base64 of the <email>:<password>), find the user associate to this
+   * email and with this password (reminder: we are storing the SHA1 of the
+   * password)
+   * - If no user has been found, return an error Unauthorized with a status
+   * code 401
+   * - Otherwise:
+   * + Generate a random string (using uuidv4) as token
+   * + Create a key: auth_<token>
+   * + Use this key for storing in Redis (by using the redisClient
+   * create previously) the user ID for 24 hours
+   * + Return this token: { "token": "155342df-2399-41da-9e8c-458b6ac52a0c" }
+   * with a status code 200
    */
   static async postNew(request, response) {
     const userEmail = request.body.email;
@@ -47,8 +47,8 @@ class UsersController {
   /**
    * GET /users/me should retrieve the user base on the token used:
    * - Retrieve the user based on the token:
-   * 	+ If not found, return an error Unauthorized with a status code 401
-   * 	+ Otherwise, return the user object (email and id only)
+   * + If not found, return an error Unauthorized with a status code 401
+   * + Otherwise, return the user object (email and id only)
    */
   static async getMe(request, response) {
     const token = request.header('X-Token') || null;
